a379d03e74e984293fd9efe41e53c87c6e838a6c
Nathan Lucca <101234415+oluccaa@users.noreply.github.com>
2026-01-09 10:57:30 -0300

    fix: Disable i18next debug logs
    Disabling the debug logs for i18next will prevent unnecessary console output in production environments, improving performance and reducing noise.

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,3 +1,4 @@
+
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
@@ -14,7 +15,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
+    debug: false,
     fallbackLng: 'pt',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
